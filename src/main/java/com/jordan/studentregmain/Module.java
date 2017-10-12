5481ec66300008c41229e241303d0b05dc0016b6
Jordan Cahill <jordancahill69@gmail.com>
2017-10-12 19:20:04 +0100

    Update code

--- src/main/java/com/jordan/studentregmain/Module.java
+++ src/main/java/com/jordan/studentregmain/Module.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.jordan.studentregmain;
 
 import java.util.ArrayList;
 
 /**
  *
- * @author Jorda
+ * @author Jordan
  */
 public class Module {
     
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,24 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 import com.jordan.studentregmain.Student;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
  *
- * @author Jorda
+ * @author Jordan
  */
 public class StudentTest {
     
     public StudentTest() {
     }
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
+    
     @Test
     public void testUsername() {
         
